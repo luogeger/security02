386736d31279b96b7de69ad25a07358a51938d85
luogeger <xiaoqing1232@163.com>
2019-06-04 12:28:18 +0800

    PasswordEncoder

--- security-browser/src/main/java/com/youyou/config/BrowserSecurityConfig.java
+++ security-browser/src/main/java/com/youyou/config/BrowserSecurityConfig.java
@@ -1,8 +1,13 @@
 package com.youyou.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * create by luoxiaoqing
@@ -10,6 +15,14 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration
 public class BrowserSecurityConfig extends WebSecurityConfigurerAdapter {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Bean// 密码加密配置
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();// 默认通过这个方法来对密码加密，也可以自定义加密对象
+    }
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
