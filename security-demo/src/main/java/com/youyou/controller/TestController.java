c0daf3ff303b881fd5deb9e18f8436786e3f5383
luogeger <xiaoqing1232@163.com>
2019-06-04 15:36:37 +0800

    认证信息的获取：SecurityContextHolder

--- security-demo/src/main/java/com/youyou/controller/TestController.java
+++ security-demo/src/main/java/com/youyou/controller/TestController.java
@@ -4,6 +4,9 @@ import com.youyou.exception.UserNotException;
 import com.youyou.pojo.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +24,28 @@ public class TestController {
 
 
     private Logger logger = LoggerFactory.getLogger(getClass());
+
+    /**
+     * 所有的认证信息
+     * @param authentication
+     * @return
+     */
+    @GetMapping("/authentication")
+    public Object authentication(Authentication authentication) {
+        return authentication;
+        //return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    /**
+     * 部分认证信息，principal，7个参数
+     * @param user
+     * @return
+     */
+    @GetMapping("/user-details")
+    public Object userDeatils(@AuthenticationPrincipal UserDetails user) {
+        return user;
+    }
+
 
 
 
